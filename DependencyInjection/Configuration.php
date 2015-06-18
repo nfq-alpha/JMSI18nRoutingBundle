15d94f197efc27eb5a0ad59e9a78e05f71a9c1b0
Saulius Vaiceliunas <saulius.vaiceliunas@nfq.lt>
2015-06-18 15:35:30 +0300

    Added tracking parameter

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -128,6 +128,7 @@ final class Configuration implements ConfigurationInterface
                                 ->end()
                                 ->scalarNode('ga_code')->defaultNull('')->end()
                                 ->scalarNode('default_currency')->defaultValue('EUR')->end()
+                                ->arrayNode('tracking')->end()
                             ->end()
                         ->end()
                     ->end()
